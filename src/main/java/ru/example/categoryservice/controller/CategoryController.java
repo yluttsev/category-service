e07da3d8c81ce42ef8b39067877f1759ca5e293c
Yuri Luttsev <floomyz@vk.com>
2025-05-11 22:57:24 +0300

    Добавлен контроллер для Category

---
+++ src/main/java/ru/example/categoryservice/controller/CategoryController.java
@@ -0,0 +1,37 @@
+package ru.example.categoryservice.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import ru.example.categoryservice.model.payload.CategoryPayload;
+import ru.example.categoryservice.model.payload.CreateCategoryRequest;
+import ru.example.categoryservice.service.CategoryService;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/categories")
+@RequiredArgsConstructor
+public class CategoryController {
+
+    private final CategoryService categoryService;
+
+    @PostMapping
+    public CategoryPayload create(@RequestBody CreateCategoryRequest createCategoryRequest) {
+        return categoryService.create(createCategoryRequest);
+    }
+
+    @GetMapping
+    public List<CategoryPayload> getAll() {
+        return categoryService.getAll();
+    }
+
+    @GetMapping("/{id}")
+    public CategoryPayload getById(@PathVariable("id") String id) {
+        return categoryService.getById(id);
+    }
+}
