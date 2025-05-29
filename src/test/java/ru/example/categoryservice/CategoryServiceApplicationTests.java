c046f13acb1563d42d67c5a770df21839ba91358
Yuri Luttsev <floomyz@vk.com>
2025-05-29 19:34:21 +0300

    Исправлен тест поднятия контекста

--- src/test/java/ru/example/categoryservice/CategoryServiceApplicationTests.java
+++ src/test/java/ru/example/categoryservice/CategoryServiceApplicationTests.java
@@ -1,10 +1,24 @@
 package ru.example.categoryservice;
 
+import com.redis.testcontainers.RedisContainer;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
 @SpringBootTest
+@Testcontainers
 class CategoryServiceApplicationTests {
+
+    @Container
+    @ServiceConnection
+    private static PostgreSQLContainer postgreSQLContainer = new PostgreSQLContainer("postgres:15");
+
+    @Container
+    @ServiceConnection
+    private static RedisContainer redisContainer = new RedisContainer("redis:7");
 
     @Test
     void contextLoads() {
