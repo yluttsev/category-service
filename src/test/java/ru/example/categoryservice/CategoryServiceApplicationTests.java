be4364a70f0b913343b0ea731b70392c62a25632
Yuri Luttsev <floomyz@vk.com>
2025-05-31 21:12:50 +0300

    Исправлен тест загрузки контекста

--- src/test/java/ru/example/categoryservice/CategoryServiceApplicationTests.java
+++ src/test/java/ru/example/categoryservice/CategoryServiceApplicationTests.java
@@ -4,11 +4,13 @@ import com.redis.testcontainers.RedisContainer;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.springframework.test.context.ActiveProfiles;
 import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
 @SpringBootTest
+@ActiveProfiles("test")
 @Testcontainers
 class CategoryServiceApplicationTests {
 
