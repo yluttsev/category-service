562063d6025cc31f166bb77f775f7d2b8d7afcd4
Yuri Luttsev <floomyz@vk.com>
2025-06-01 16:58:53 +0300

    Добавлена защита эндпоинта POST /categories

--- src/main/java/ru/example/categoryservice/controller/CategoryController.java
+++ src/main/java/ru/example/categoryservice/controller/CategoryController.java
@@ -1,6 +1,7 @@
 package ru.example.categoryservice.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -21,6 +22,7 @@ public class CategoryController {
     private final CategoryService categoryService;
 
     @PostMapping
+    @PreAuthorize("hasRole('ADMIN')")
     public CategoryPayload create(@RequestBody CreateCategoryRequest createCategoryRequest) {
         return categoryService.create(createCategoryRequest);
     }
